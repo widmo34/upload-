d4f18d97dacb7acfc8b3eb07f5e5d263c8e33a02
Radoslaw Weidemann <walecznyopos@gmail.com>
2018-07-04 13:57:07 +0200

    13.9

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,12 +1,22 @@
-var fs = require('fs');
+// var fs = require('fs');
+const fs = require('fs-extra')
+
 var formidable = require('formidable');
 
 exports.upload = function(request, response) {
     console.log("Rozpoczynam obsługę żądania upload.");
     var form = new formidable.IncomingForm();
     form.parse(request, function(error, fields, files) {
-                
-        fs.renameSync(files.upload.path, "test.png");
+        
+        console.log(files.upload.path);
+
+        try {
+            fs.copySync(files.upload.name, 'test.png')
+            console.log('success!')
+          } catch (err) {
+            console.error(err)
+          }
+        //fs.renameSync(files.upload.path, "test.png");
         response.writeHead(200, {"Content-Type": "text/html"});
         response.write("received image:<br/>");
         response.write("<img src='/show' />");
@@ -33,6 +43,7 @@ exports.error = function(request, response) {
 }
 
 exports.show = function(request, response) {
+           
     fs.readFile("test.png", "binary", function(error, file) {
         response.writeHead(200, {"Content-Type": "image/png"});
         response.write(file, "binary");
