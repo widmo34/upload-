a10af383b735f5024ad7630bef43fccc02ab0aab
Radoslaw Weidemann <walecznyopos@gmail.com>
2018-07-04 12:20:00 +0200

    Add basic files

---
+++ modules/server.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var colors = require('colors');
+var formidable = require('formidable');
+var handlers = require('./handlers'); // modules
+
+function start() {
+  function onRequest(request, response) {
+
+   
+    response.writeHead(200, {"Content-Type": "text/plain ; charset=utf-8"});
+   
+
+    
+    switch (request.url) { // switch rozróżniający zapytania
+        case '/':
+        case '/start':
+            handlers.welcome(request, response);
+            break;
+        case '/upload':
+            handlers.upload(request, response);
+            break;
+        case '/show':
+            handlers.show(request, response);
+            break;    
+        default:
+            handlers.error(request, response);
+    }
+  }
+
+  http.createServer(onRequest).listen(9000);
+
+  console.log("Uruchomiono serwer :) !".green);
+}
+
+exports.start = start;
